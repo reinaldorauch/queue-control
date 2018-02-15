478db9bdfb0b9bd63dcb17451a89142dcb65e3dd
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2018-02-15 03:43:29 +0000

    Adicionado propriedade de mesa no template

--- libs/root-handler.js
+++ libs/root-handler.js
@@ -1,11 +1,11 @@
 const rootHandler = require('express').Router();
 
 rootHandler.get('/', (req, res) => {
-  res.render('pages/index', { counter: 0 });
+  res.render('pages/index', { counter: 0, mesa: '' });
 });
 
 rootHandler.get('/mesa', (req, res) => {
-  res.render('pages/mesa', { counter: 0 });
-})
+  res.render('pages/mesa', { counter: 0, mesa: '' });
+});
 
 module.exports = rootHandler;
