47e392eb0aa0dd41752e373eb3b6d5011e1d6242
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2018-02-15 17:47:07 -0200

    Adicionado persistência do último guichê usado

--- public/mesa.js
+++ public/mesa.js
@@ -1,25 +1,33 @@
 (function (document, io, undefined) {
   'use strict';
 
+  const LOCALSTORAGE_ULTIMA_MESA = 'mesa';
+
   const $ = x => document.querySelector(x);
   const senha = $('#senha');
   const mesaOrigem = $('#mesa-origem');
   const call = $('#call');
   const reset = $('#reset');
-  const mesa = () => $('#mesa').value;
+  const mesa =  $('#mesa');
+  const mesaVal = () => mesa.value;
 
   const socket = io({ transports: ['websocket'], upgrade: false });
 
   socket.on('update-queue', function (queue) {
-    console.log('Received update queue', queue);
     senha.innerText = queue.counter;
     mesaOrigem.innerText = queue.mesa;
   });
 
   call.addEventListener('submit', e => {
+    const guiche = mesaVal();
     e.preventDefault();
-    socket.emit('request-update-queue', mesa());
+    localStorage.setItem(LOCALSTORAGE_ULTIMA_MESA, guiche);
+    socket.emit('request-update-queue', guiche);
   });
   reset.addEventListener('click', () => socket.emit('reset'));
+
+  document.addEventListener('readystatechange', () => {
+    mesa.value = localStorage.getItem(LOCALSTORAGE_ULTIMA_MESA);
+  });
 
 })(document, io);
